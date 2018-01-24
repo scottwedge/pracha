00af074fd70e27135b5efbac30569a123ebdf47a
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-01-24 18:47:03 +0530

    adding  pdf lib
    adding  pdf lib

--- application/controllers/Seo.php
+++ application/controllers/Seo.php
@@ -3,21 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Seo extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct() 
+		{
+
+		parent::__construct();	
+		$this->load->helper(array('url','html','form'));
+		$this->load->library('session','form_validation');
+		$this->load->library('email');
+		$this->load->library('pdf');
+	}
 	public function index()
 	{
 		$this->load->view('header');
@@ -41,6 +35,27 @@ class Seo extends CI_Controller {
 	$this->load->view('header');
 	$this->load->view('affilatemanagement');
 	$this->load->view('footer');
+	}
+	public function affilatemanagementpdf()
+	{
+		 $path = rtrim(FCPATH,"/");
+		//echo '<pre>';print_r($list);exit;
+		//echo '<pre>';print_r($data['details']);exit;
+		$file_name = 'affilatemanagement.pdf'; 
+		$data['name']=$file_name;		
+		$pdfFilePath = $path."/assets/downloads/".$file_name;
+		ini_set('memory_limit','320M'); // boost the memory limit if it's low <img src="https://s.w.org/images/core/emoji/72x72/1f609.png" alt="??" draggable="false" class="emoji">
+		$html =$this->load->view('affilatemanagement',$data, true); // render the view into HTML
+		//echo '<pre>';print_r($html);exit;
+		$stylesheet1 = file_get_contents(base_url('assets/vendor/css/bootstrap.min.css')); // external css
+		$stylesheet6 = file_get_contents('https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+		$this->load->library('pdf');
+		$pdf = $this->pdf->load();
+		$pdf->SetFooter($_SERVER['HTTP_HOST'].'|{PAGENO}|'.date('M-d-Y')); // Add a footer for good measure <img src="https://s.w.org/images/core/emoji/72x72/1f609.png" alt="??" draggable="false" class="emoji">
+		$pdf->SetDisplayMode('fullpage');
+		$pdf->list_indent_first_level = 0;	// 1 or 0 - whether to indent the first level of a list
+		$pdf->WriteHTML($html); // write the HTML into the PDF
+		$pdf->Output($pdfFilePath, 'F'); 
 	}
 	
 	
