2bb36aedc079dc005f3884e026b984726578b134
bayapucg <vasudevareddy549@gmail.com>
2018-01-29 14:55:30 +0530

    commit
    pfrofile

--- application/views/profile.php
+++ application/views/profile.php
@@ -1,81 +1,173 @@
-
-<section id="team-scroll" class="our_service padding-10 ptb-xs-60 bod-st  form-elegant">
-  
-
-    <!--Form without header-->
-    <div class="  row justify-content-md-center">
-
-        <div class="card-body mx-4 card col-4 ">
-
-            <!--Header-->
-            <div class="text-center">
-                <h3 class="dark-grey-text mb-5"><strong>Sign in</strong></h3>
-            </div>
-
-            <!--Body-->
-			<form action="<?php echo base_url('employee/loginpost'); ?>" method="post" >
-            <div class="md-form">
-                <input type="text" id="emp_email" name="emp_email"  class="form-control">
-                <label for="Form-email1">Your email</label>
-            </div>
-
-            <div class="md-form pb-3">
-                <input type="password" id="emp_password" name="emp_password" class="form-control">
-                <label for="Form-pass1">Your password</label>
-                <p class="font-small blue-text d-flex justify-content-end">Forgot <a href="#" class="blue-text ml-1"> Password?</a></p>
-            </div>
-
-            <div class="text-center mb-3">
-                <button type="submit" class="btn blue-gradient btn-block btn-rounded z-depth-1a">Sign in</button>
-            </div>
-			</form>
-
-        </div>
-		<div class="clearfix"> &nbsp;</div>
-		
-		
-			<!--forget password-->
-		<div class="card-body mx-4 card col-4 ">
-			<form>
-				<p class="h5 text-center mb-4">Forgot Password</p>
-
-				<div class="md-form">
-					<i class="fa fa-user prefix grey-text"></i>
-					<input type="text" id="orangeForm-name" class="form-control">
-					<label for="orangeForm-name">Your name</label>
+<section class="  section magazine-section bod-st   "style="margin-top:160px;">
+	<div class=" container card " style="padding:10px;">
+		<!--Grid row-->
+		<div class="row text-left">
+			<!--Grid column-->
+			<div class="col-lg-6 col-md-12">
+				<!--Featured news-->
+				<div class="single-news">
+					<!--Image-->
+					<div class="view overlay hm-white-slight rounded z-depth-1-half mb-3">
+							<div class="">
+    
+								<!--Card-->
+								<div class="card testimonial-card">
+						
+									<!--Background color-->
+									<div class="card-up info-color"></div>
+						
+									<!--Avatar-->
+									<div class="avatar">
+										<img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg" class="rounded-circle img-responsive">
+									</div>
+						
+									<div class="card-body">
+										<!--Name-->
+										<h4 class="mt-1">
+											<strong>John Doe</strong>
+										</h4>
+										<hr>
+										<!--Quotation-->
+										<p class="dark-grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci.</p>
+									</div>
+						
+								</div>
+								<!--Card-->
+						
+							</div>
+					</div>
+					<!--Excerpt-->
+					<div class="news-data mb-5">
+						<a href="" class="deep-orange-text">
+							<h6>
+								<i class="fa fa-cutlery"></i><strong> Culinary</strong>
+							</h6>
+						</a>
+						<p class="dark-grey-text">
+<strong>
+ <i class="fa fa-clock-o"></i> 27/02/2018</strong>
+						</p>
+					</div>
+					<h3 class="pt-2">
+ <a>
+ <strong>This is title
+of the news</strong>
+ </a>
+ </h3>
+					<p align="justify">Sed ut perspiciatis unde voluptatem omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae explicabo. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 				</div>
-				<div class="md-form">
-					<i class="fa fa-envelope prefix grey-text"></i>
-					<input type="text" id="orangeForm-email" class="form-control">
-					<label for="orangeForm-email">Your email</label>
+				<!--/Featured news-->
+			</div>
+			<!--Grid column-->
+			<!--Grid column-->
+			<div class="col-lg-6 col-md-12">
+				<!--Small news-->
+				<div class="single-news">
+				<h3 class="dark-grey-text"> <strong>Heading</strong></h3>
+					<div class="row">
+					
+						<div class="col-md-3">
+							<!--Image-->
+							<div class="view overlay hm-white-slight rounded z-depth-1">
+								<img src="https://mdbootstrap.com/img/Photos/Others/img%20(29).jpg" class="img-fluid" alt="Minor sample
+post image">
+								<a>
+									<div class="mask"></div>
+								</a>
+							</div>
+						</div>
+						<!--Excerpt-->
+						<div class="col-md-9">
+							<p class="dark-grey-text"> <strong>25/02/2018</strong>
+							</p>
+<a class="grey-text">Duis aute irure dolor in reprehenderit in voluptate.
+ <i class="fa
+fa-angle-right"></i>
+ </a>
+						</div>
+					</div>
 				</div>
-
-				<div class="md-form">
-					<i class="fa fa-lock prefix grey-text"></i>
-					<input type="password" id="orangeForm-pass" class="form-control">
-					<label for="orangeForm-pass"> Password</label>
+				<!--/Small news-->
+				<!--Small news-->
+				<div class="single-news">
+					<div class="row">
+						<div class="col-md-3">
+							<!--Image-->
+							<div class="view overlay hm-white-slight rounded z-depth-1">
+								<img src="https://mdbootstrap.com/img/Photos/Horizontal/Food/4-col/img%20(45).jpg" class="img-fluid" alt="Minor sample post image">
+								<a>
+									<div class="mask"></div>
+								</a>
+							</div>
+						</div>
+						<!--Excerpt-->
+						<div class="col-md-9">
+							<p class="dark-grey-text"> <strong>25/02/2018</strong>
+							</p>
+<a class="grey-text">Duis aute irure dolor in reprehenderit in voluptate.
+ <i class="fa
+fa-angle-right"></i>
+ </a>
+						</div>
+					</div>
 				</div>
-				<div class="md-form">
-					<i class="fa fa-lock prefix grey-text"></i>
-					<input type="password" id="orangeForm-pass" class="form-control">
-					<label for="orangeForm-pass">Confirm Password</label>
+				<!--/Small news-->
+				<!--Small news-->
+				<div class="single-news">
+				<h4> <strong>Heading</strong></h4>
+						
+					<div class="row">
+						
+						<div class="col-md-3">
+							<!--Image-->
+							<div class="view overlay hm-white-slight rounded z-depth-1">
+								<img src="https://mdbootstrap.com/img/Photos/Others/images/87.jpg" class="img-fluid" alt="Minor sample post image">
+								<a>
+									<div class="mask"></div>
+								</a>
+							</div>
+						</div>
+						<!--Excerpt-->
+						<div class="col-md-9">
+						
+							<p class="dark-grey-text"> <strong>25/02/2018</strong>
+							</p>
+							<a class="grey-text">Duis aute irure dolor in reprehenderit in voluptate.
+							 <i class="fa
+							fa-angle-right"></i>
+							 </a>
+						</div>
+					</div>
 				</div>
-
-				<div class="text-center mb-3">
-					<button type="button" class="btn blue-gradient btn-block btn-rounded z-depth-1a">Reset Password</button>
+				<!--/Small news-->
+				<!--Small news-->
+				<div class="single-news">
+					<div class="row">
+						<div class="col-md-3">
+							<!--Image-->
+							<div class="view overlay hm-white-slight rounded z-depth-1">
+								<img src="https://mdbootstrap.com/img/Photos/Others/img%20(27).jpg" class="img-fluid" alt="Minor sample post image">
+								<a>
+									<div class="mask"></div>
+								</a>
+							</div>
+						</div>
+						<!--Excerpt-->
+						<div class="col-md-9">
+							<p class="dark-grey-text"> <strong>25/02/2018</strong>
+							</p>
+<a class="grey-text">Duis aute irure dolor in reprehenderit in voluptate.
+ <i class="fa
+fa-angle-right"></i>
+ </a>
+						</div>
+					</div>
 				</div>
-
-			</form>
-			
-			
-			
-			
-
-        </div>
-
-     
-    </div>
-    <!--/Form without header-->
-
+				<!--/Small news-->
+			</div>
+			<!--Grid column-->
+		</div>
+	</div>
+	<!--Grid row-->
 </section>
-            
+<!--Section: Magazine v.2-->
