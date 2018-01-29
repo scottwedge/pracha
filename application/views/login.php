28c47ed0c09b94518becf68e20a1d894685f6dc5
bayapucg <vasudevareddy549@gmail.com>
2018-01-29 13:04:19 +0530

    commit
    change

--- application/views/login.php
+++ application/views/login.php
@@ -1,187 +1,79 @@
-<?php include('header.php'); ?>
-<section id="team-scroll" class="our_service padding-10 ptb-xs-60">
-    <div class="container-sty card ">
-			<div class="row  text-center">
-            <div class="col-sm-12 mb-20 pt-30">
 
-                <h2 class="tex-lef-mob">
-                    <span class="tit">
-									<span class="des_t">Our</span> Team
-                    </span>
-                </h2>
+<section id="team-scroll" class="our_service padding-10 ptb-xs-60 bod-st  form-elegant">
+  
 
+    <!--Form without header-->
+    <div class="  row justify-content-md-center">
 
+        <div class="card-body mx-4 card col-4 ">
+
+            <!--Header-->
+            <div class="text-center">
+                <h3 class="dark-grey-text mb-5"><strong>Sign in</strong></h3>
+            </div>
+
+            <!--Body-->
+            <div class="md-form">
+                <input type="text" id="Form-email1" class="form-control">
+                <label for="Form-email1">Your email</label>
+            </div>
+
+            <div class="md-form pb-3">
+                <input type="password" id="Form-pass1" class="form-control">
+                <label for="Form-pass1">Your password</label>
+                <p class="font-small blue-text d-flex justify-content-end">Forgot <a href="#" class="blue-text ml-1"> Password?</a></p>
+            </div>
+
+            <div class="text-center mb-3">
+                <button type="button" class="btn blue-gradient btn-block btn-rounded z-depth-1a">Sign in</button>
             </div>
 
         </div>
-        <!--Carousel Wrapper-->
-        <div id="multi-item-example" class="carousel slide carousel-multi-item" data-ride="carousel">
+		<div class="clearfix"> &nbsp;</div>
+		
+		
+			<!--forget password-->
+		<div class="card-body mx-4 card col-4 ">
+			<form>
+				<p class="h5 text-center mb-4">Forgot Password</p>
 
-            <!--Controls-->
-            <div class="controls-top">
-                <a class="btn-floating" href="#multi-item-example" data-slide="prev"><i class="fa fa-chevron-left"></i></a>
-                <a class="btn-floating" href="#multi-item-example" data-slide="next"><i class="fa fa-chevron-right"></i></a>
-            </div>
-            <!--/.Controls-->
+				<div class="md-form">
+					<i class="fa fa-user prefix grey-text"></i>
+					<input type="text" id="orangeForm-name" class="form-control">
+					<label for="orangeForm-name">Your name</label>
+				</div>
+				<div class="md-form">
+					<i class="fa fa-envelope prefix grey-text"></i>
+					<input type="text" id="orangeForm-email" class="form-control">
+					<label for="orangeForm-email">Your email</label>
+				</div>
 
-            <!--Indicators--><!--
-            <ol class="carousel-indicators">
-                <li data-target="#multi-item-example" data-slide-to="0" class="active"></li>
-                <li data-target="#multi-item-example" data-slide-to="1"></li>
-                
-            </ol>-->
-            <!--/.Indicators-->
+				<div class="md-form">
+					<i class="fa fa-lock prefix grey-text"></i>
+					<input type="password" id="orangeForm-pass" class="form-control">
+					<label for="orangeForm-pass"> Password</label>
+				</div>
+				<div class="md-form">
+					<i class="fa fa-lock prefix grey-text"></i>
+					<input type="password" id="orangeForm-pass" class="form-control">
+					<label for="orangeForm-pass">Confirm Password</label>
+				</div>
 
-            <!--Slides-->
-            <div class="carousel-inner" role="listbox">
+				<div class="text-center mb-3">
+					<button type="button" class="btn blue-gradient btn-block btn-rounded z-depth-1a">Reset Password</button>
+				</div>
 
-                <!--First slide-->
-                <div class="carousel-item active">
-
-                    <div class="col-md-4">
-                        <div class="card testimonial-card">
-                            <!--Bacground color-->
-                            <div class="card-up indigo lighten-1">
-                            </div>
-
-                            <!--Avatar-->
-                            <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
-                            </div>
-
-                            <div class="card-body">
-                                <!--Name-->
-                                <h4 class="card-title">Anna Doe</h4>
-                                <hr>
-                                <!--Quotation-->
-                                <p><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 clearfix d-none d-md-block">
-                        <div class="card testimonial-card">
-
-                            <!--Bacground color-->
-                            <div class="card-up indigo lighten-1">
-                            </div>
-
-                            <!--Avatar-->
-                            <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
-                            </div>
-
-                            <div class="card-body">
-                                <!--Name-->
-                                <h4 class="card-title">Anna Doe</h4>
-                                <hr>
-                                <!--Quotation-->
-                                <p><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 clearfix d-none d-md-block">
-                        <div class="card">
-                            <div class="card testimonial-card">
-
-                                <!--Bacground color-->
-                                <div class="card-up indigo lighten-1">
-                                </div>
-
-                                <!--Avatar-->
-                                <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
-                                </div>
-
-                                <div class="card-body">
-                                    <!--Name-->
-                                    <h4 class="card-title">Anna Doe</h4>
-                                    <hr>
-                                    <!--Quotation-->
-                                    <p><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <!--/.First slide-->
-
-                <!--Second slide-->
-                <div class="carousel-item">
-
-                    <div class="col-md-4">
-                        <div class="card testimonial-card">
-
-                            <!--Bacground color-->
-                            <div class="card-up indigo lighten-1">
-                            </div>
-
-                            <!--Avatar-->
-                            <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
-                            </div>
-
-                            <div class="card-body">
-                                <!--Name-->
-                                <h4 class="card-title">Anna Doe</h4>
-                                <hr>
-                                <!--Quotation-->
-                                <p><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 clearfix d-none d-md-block">
-                        <div class="card testimonial-card">
-
-                            <!--Bacground color-->
-                            <div class="card-up indigo lighten-1">
-                            </div>
-
-                            <!--Avatar-->
-                            <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
-                            </div>
-
-                            <div class="card-body">
-                                <!--Name-->
-                                <h4 class="card-title">Anna Doe</h4>
-                                <hr>
-                                <!--Quotation-->
-                                <p><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-md-4 clearfix d-none d-md-block">
-                        <div class="card testimonial-card">
-
-                            <!--Bacground color-->
-                            <div class="card-up indigo lighten-1">
-                            </div>
-
-                            <!--Avatar-->
-                            <div class="avatar"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
-                            </div>
-
-                            <div class="card-body">
-                                <!--Name-->
-                                <h4 class="card-title">Anna Doe</h4>
-                                <hr>
-                                <!--Quotation-->
-                                <p><i class="fa fa-quote-left"></i> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, adipisci</p>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <!--/.Second slide-->
-
-
-
-            </div>
-            <!--/.Slides-->
+			</form>
+			
+			
+			
+			
 
         </div>
+
+     
     </div>
-    <!--/.Carousel Wrapper-->
-	<div class="clearfix">&nbsp;</div>
+    <!--/Form without header-->
+
 </section>
-<?php include('footer.php'); ?>
+            
