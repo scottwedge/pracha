fbfce62e800b0eecf15e06eab24b12852e470e3c
bayapucg <vasudevareddy549@gmail.com>
2018-02-05 17:47:39 +0530

    commit
    changes

--- application/views/suggestion.php
+++ application/views/suggestion.php
@@ -1,6 +1,122 @@
 
 <div class="container-sty-adm ">
-	cooming soon
+	<div class=" row justify-content-md-center">
+		<div class="col-md-5  " style="">
+			
+			 <!-- DIRECT CHAT -->
+              <div class="box box-warning direct-chat direct-chat-primary card">
+                <div class="box-header with-border bg-warning text-white">
+                  <h3 class="box-title">Direct Chat</h3>
+
+                 
+                </div>
+				
+                <div class="box-body">
+                  <!-- Conversations are loaded here -->
+                  <div class="direct-chat-messages">
+                  
+                      <div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">ffusdfuysdayuf</span>
+                        <span class="direct-chat-timestamp pull-left">hi kfhsfkskdf</span>
+                      </div>
+                      
+					   <img class="direct-chat-img" src="" alt=""><!-- /.direct-chat-img -->
+						
+						<div class="direct-chat-text">
+                        fsdjfsjdfsd
+                      </div>
+                    </div>
+					 <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">fsdafs </span>
+                        <span class="direct-chat-timestamp pull-right">25/04/20018</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+					
+					       <img class="direct-chat-img" src="" alt="test"><!-- /.direct-chat-img -->
+					  
+                      <div class="direct-chat-text">
+                        gguugsyudgysdyu second
+                      </div>
+                    </div>
+					<div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">ffusdfuysdayuf</span>
+                        <span class="direct-chat-timestamp pull-left">hi kfhsfkskdf</span>
+                      </div>
+                      
+					   <img class="direct-chat-img" src="" alt=""><!-- /.direct-chat-img -->
+						
+						<div class="direct-chat-text">
+                        fsdjfsjdfsd
+                      </div>
+                    </div>
+					 <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">fsdafs </span>
+                        <span class="direct-chat-timestamp pull-right">25/04/20018</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+					
+					       <img class="direct-chat-img" src="" alt="test"><!-- /.direct-chat-img -->
+					  
+                      <div class="direct-chat-text">
+                        gguugsyudgysdyu second
+                      </div>
+                    </div>
+					<div class="direct-chat-msg right">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-right">ffusdfuysdayuf</span>
+                        <span class="direct-chat-timestamp pull-left">hi kfhsfkskdf</span>
+                      </div>
+                      
+					   <img class="direct-chat-img" src="" alt=""><!-- /.direct-chat-img -->
+						
+						<div class="direct-chat-text">
+                        fsdjfsjdfsd
+                      </div>
+                    </div>
+					 <div class="direct-chat-msg">
+                      <div class="direct-chat-info clearfix">
+                        <span class="direct-chat-name pull-left">fsdafs </span>
+                        <span class="direct-chat-timestamp pull-right">25/04/20018</span>
+                      </div>
+                      <!-- /.direct-chat-info -->
+					
+					       <img class="direct-chat-img" src="" alt="test"><!-- /.direct-chat-img -->
+					  
+                      <div class="direct-chat-text">
+                        gguugsyudgysdyu second
+                      </div>
+                    </div>
+					
+                    
+                    <!-- /.direct-chat-msg -->
+
+                  </div>
+                  <!--/.direct-chat-messages-->
+
+                 
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+				<div class="row">
+                      <div class="col-md-8">
+						<input type="text"  id="message" name="message" placeholder="Message ..." class="form-control">
+					  </div>
+                          
+                      <div class="col-md-2"> <button type="submit" class="btn btn-dark ">Send</button>
+					  </div>
+				 </div>
+                
+                </div>
+                <!-- /.box-footer-->
+              </div>
+        
+
+		</div>
+	</div>
 </div>
 
 	
