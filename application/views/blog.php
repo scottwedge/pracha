f13b5e473c8975c19da2cdc5b378d264f64f7a15
bayapucg <vasudevareddy549@gmail.com>
2018-04-07 19:03:34 +0530

    commit blog
    designe changes

--- application/views/blog.php
+++ application/views/blog.php
@@ -15,8 +15,10 @@
 
 
     <!--Grid row-->
-    <div class="row justify-content-md-center pt-5 mt-4 help-blog">
-        <div class="col-lg-3 col-md-3 card cust-wid">
+    <div class="row justify-content-md-center  mt-4 ">
+        <div class="col-lg-4 col-xs-12 ">
+        <div class="card  pad-10">
+		
             <!--Featured image-->
             <div class="view overlay rounded z-depth-2 mb-2">
                 <img src="<?php echo base_url(); ?>assets/vendor/img/blog-you.png" class="img-fluid" alt="Blog">
@@ -40,7 +42,9 @@
 				</div>
 			</div>
         </div> 
-		<div class="col-lg-3 col-md-3  card cust-wid">
+        </div> 
+		<div class="col-lg-4 col-xs-12   ">
+			<div class="card  pad-10">
             <!--Featured image-->
             <div class="view overlay rounded z-depth-2 mb-2">
                 <img src="<?php echo base_url(); ?>assets/vendor/img/onpage-and-offpage-optimization-blog.png" class="img-fluid" alt="Blog2">
@@ -65,7 +69,9 @@
 				</div>
 			</div>
         </div>
-		<div class="col-lg-3 col-md-3  card cust-wid">
+        </div>
+		<div class="col-lg-4 col-xs-12   ">
+			<div class="card  pad-10">
             <!--Featured image-->
             <div class="view overlay rounded z-depth-2 mb-2">
                 <img src="<?php echo base_url(); ?>assets/vendor/img/social-media-marketing-blog.png" class="img-fluid" alt="Blog2">
@@ -91,12 +97,14 @@
 				</div>
 			</div>
         </div>
+        </div>
         <!--Grid column-->
 
 
     </div>
 	<div class="row justify-content-md-center pt-5 mt-4 help-blog">
-        <div class="col-lg-3 col-md-3 card cust-wid">
+        <div class="col-lg-4 col-xs-12  ">
+		<div class="card  pad-10">
             <!--Featured image-->
             <div class="view overlay rounded z-depth-2 mb-2">
                 <img src="<?php echo base_url(); ?>assets/vendor/img/what-is-ecommerce.png" class="img-fluid" alt="Blog">
@@ -113,7 +121,7 @@
             <p class="grey-text">eCommerce is huge and it is ever growing. Have you ever purchased goods on the internet? If yes, then assuredly you have played an active role in the world of e-commerce; a world where the transactions are done digitally, and the products are conveniently delivered at the doorstep of the consumer. </p>
 			<div class="row">
 				<div class="col-md-7 ">
-					<p class="date-design">30-03-2018</p>
+					<p class="date-design">30-March-2018</p>
 				</div>
 				<div class="col-md-5 ">
 					<a href="<?php echo base_url('blog/what_is_ecommerce_and_its_types');?>" class="btn btn-warning btn-rounded btn-sm pull-right">Read more</a>
